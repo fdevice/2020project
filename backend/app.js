f3248b1e9ca02b7bc5d2d3e078b89200ca57d47f
Dominik W <d.p.wysocki@gmail.com>
2022-06-05 19:02:46 +0200

    commit 2022

--- backend/app.js
+++ backend/app.js
@@ -8,8 +8,8 @@ const app = express();
 
 mongoose
   .connect(        
-    // "mongodb+srv://dominik:njy3eMG6Uph1MBAf@cluster0.usbpw.mongodb.net/test?retryWrites=true&w=majority",
-    "mongodb+srv://dominik:" + process.env.MONGO_ATLAS_PW + "@cluster0.usbpw.mongodb.net/test?retryWrites=true&w=majority",
+    "mongodb+srv://Dominik:8HX0EstG4sZKtln9@cluster0.skyji.mongodb.net/?retryWrites=true&w=majority",
+    // "mongodb+srv://dominik:" + process.env.MONGO_ATLAS_PW + "@cluster0.usbpw.mongodb.net/test?retryWrites=true&w=majority",
     { 
       useNewUrlParser: true,
       useUnifiedTopology: true,
